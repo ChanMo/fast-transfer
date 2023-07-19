cee30adaf15bfa18ee7d68396d2adbd08b5ae300
dyou <chen.orange@aliyun.com>
2023-07-19 15:57:18 +0800

    use hatch to build and publish

---
+++ README.md
@@ -0,0 +1,17 @@
+# Fast Transfer
+
+An easy and fast way to transfer files between mulitple users.
+If you offten run the command `python -m http.server`, you will like it.
+
+
+## Usage
+
+```
+fast-transfer /tmp
+```
+
+## Installation
+
+```
+pip install fast-transfer
+```
--- src/fast_transfer/app.py
+++ src/fast_transfer/app.py
@@ -2,46 +2,46 @@ import os
 import uuid
 import datetime
 import flask
+import click
 
 app = flask.Flask(__name__)
 app.secret_key = str(uuid.uuid4())
 
-directory = os.environ.get('FLASK_USER_DATA', os.getcwd())
-if not directory.startswith("/"):
-    directory = os.path.join(os.getcwd(), directory)
 
-    
 @app.route("/", methods=["GET", "POST"])
 def index():
-    dir = flask.request.args.get('dir', '')
-    # Get the list of files in the current directory
-    dir2 = os.path.join(directory, dir)
+    root = app.config['USER_DATA']
+    if not root.startswith("/"):
+        root = os.path.join(os.getcwd(), root)
+    
+    folder = flask.request.args.get('dir', '')
+    path = os.path.join(root, folder)
     
     if flask.request.method == "POST":
         # Get the file from the client
         file = flask.request.files["file"]
 
         # Save the file to the server
-        file_path = os.path.join(dir2, file.filename)
+        file_path = os.path.join(path, file.filename)
         file.save(file_path)
 
         flask.flash('Upload Successfully.')
 
-        return flask.redirect(f'/?dir={dir}')
+        return flask.redirect(f'/?dir={folder}')
 
     if flask.request.args.get('file'):
         f = flask.request.args.get('file')
-        return flask.send_from_directory(dir2, f, as_attachment=True)
+        return flask.send_from_directory(path, f, as_attachment=True)
 
-    files = os.listdir(dir2)
+    files = os.listdir(path)
 
     # Create a list of links to the files
     links = []
     for file in files:
-        f = os.path.join(dir2, file)
+        f = os.path.join(path, file)
         isdir = os.path.isdir(f)
-        dir_link = f"?dir={dir}/{file}" if dir else f'?dir={file}'
-        file_link = f'?dir={dir}&file={file}'
+        dir_link = f"?dir={folder}/{file}" if folder else f'?dir={file}'
+        file_link = f'?dir={folder}&file={file}'
         links.append({
             'file': file if not isdir else f'{file}/',
             'link': dir_link if isdir else file_link,
@@ -52,15 +52,24 @@ def index():
         })
 
     dirs = []
-    if dir:
-        for i in dir.split('/'):
+    if folder:
+        for i in folder.split('/'):
             dirs.append({
                 'name': i,
-                'path': f'{dirs[-1]["path"]}/{i}' if len(dirs) else i
+                'path': f'{dirs[-1]["path"]}/{i}' if len(dirs) > 0 else i
             })
 
     return flask.render_template("index.html", links=links, dirs=dirs)
 
 
+@click.command()
+@click.argument('userdata')
+def cli(userdata):
+    app.config.update(
+        USER_DATA=userdata
+    )
+    app.run(host='0.0.0.0')
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    cli()
